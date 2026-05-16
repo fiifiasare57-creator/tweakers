e61a3cee5257f8dcb2dfe94bf99a23cba95258d9
fiifiasare57-creator <fiifiasare57@gmail.com>
2026-05-17 00:45:00 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import VideoFeed from './components/VideoFeed';
+
+function App() {
+  return (
+    <div className="h-screen bg-black">
+      <VideoFeed />
+    </div>
+  );
+}
+
+export default App;
